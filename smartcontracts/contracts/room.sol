44ce1ee2fe5550c920499d5afdb5c9b7fe6424d7
Nandika-A <nandikaagrawal610@gmail.com>
2024-04-20 01:49:46 +0530

    added push and retrieve functions

--- smartcontracts/contracts/room.sol
+++ smartcontracts/contracts/room.sol
@@ -2,14 +2,39 @@
 pragma solidity ^0.8.0;
 
 contract Room {
+
+    event NewCommit(uint id);
+
     struct Commit {
         address user;
         uint timestamp;
         string model;
     }
+
     Commit[] private commits;
 
     mapping (uint => uint) private commitToRoom;
     mapping (uint => uint) private roomCommitCount;
-    mapping (address => uint[]) private memberToRoom;
+
+    function push (string memory _model, uint _room) external {
+        commits.push(Commit(msg.sender, block.timestamp, _model));
+        uint id = commits.length - 1;
+        commitToRoom[id] = _room;
+        roomCommitCount[_room] ++;
+        emit NewCommit(id);
+    } 
+
+    function retrieve (uint _room) external view returns (Commit[] memory) {
+        Commit[] memory result = new Commit[](roomCommitCount[_room]);
+        uint counter = 0;
+        for(uint i = 0; i < commits.length; i++)
+        {
+            if(keccak256(abi.encodePacked(commitToRoom[i])) == keccak256(abi.encodePacked(_room)))
+            {
+                result[counter] = commits[i];
+                counter ++;
+            }
+        }
+        return result;
+    }
 }
