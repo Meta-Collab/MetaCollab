0ed8973cbeda88d2460392f4286694e21fdd5840
eleensmathew <eleensmathew@gmail.com>
2024-04-20 02:51:53 +0530

    made backend urls and models

--- metacollab/main/views.py
+++ metacollab/main/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import User, Room, RoomToUser
 
 # Create your views here.
+
+def get_room(request,roomuuid):
+    pass
+
+def api_list_room(request):
+    pass
+
+def list_room(request):
+    pass
+
+
+def home(request):
+    pass
