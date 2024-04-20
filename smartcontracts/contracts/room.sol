a7f312dc989043ada1dc412fd006893c674c35e8
Nandika-A <nandikaagrawal610@gmail.com>
2024-04-20 06:46:34 +0530

    completed smart contract

--- smartcontracts/contracts/room.sol
+++ smartcontracts/contracts/room.sol
@@ -11,12 +11,32 @@ contract Room {
         string model;
     }
 
+    modifier onlyMember(uint _room)
+    {
+        uint set = 0;
+        for(uint i = 0; i < memberToRoom[msg.sender].length; i++)
+        {
+            if(memberToRoom[msg.sender][i] == _room)
+                set = 1;
+        }
+        require(set == 1);
+        _;
+    }
+
     Commit[] private commits;
 
     mapping (uint => uint) private commitToRoom;
     mapping (uint => uint) private roomCommitCount;
+    mapping (address => uint[]) private memberToRoom;
 
-    function push (string memory _model, uint _room) external {
+    function create_room (uint _room, address[] memory _users) external {
+        for(uint i = 0; i < _users.length; i++)
+        {
+            memberToRoom[_users[i]].push(_room);
+        }
+    }
+
+    function push (string memory _model, uint _room) external onlyMember(_room) {
         commits.push(Commit(msg.sender, block.timestamp, _model));
         uint id = commits.length - 1;
         commitToRoom[id] = _room;
