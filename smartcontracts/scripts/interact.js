96e1d8f02fef540e39f5890e999189e099affede
Nandika-A <nandikaagrawal610@gmail.com>
2024-04-20 11:16:56 +0530

    added main script

--- smartcontracts/scripts/interact.js
+++ smartcontracts/scripts/interact.js
@@ -26,3 +26,19 @@ async function get_commits(room_id)
   return commits
 }
 
+// async function main() {
+//     const storageFactory = await ethers.getContractFactory("Room")
+//     const contract = await storageFactory.attach(CONTRACT_ADDRESS);
+//     await contract.create_room(1, ["0xe2BA10C388ef4A013Db4ff13f56B742893208D05"])
+//     await contract.push("abc", 1)
+//     console.log("successfully updated contract")
+//     commits = [].slice.call(await contract.retrieve(1))
+//     console.log(commits)
+// }
+
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error)
+//     process.exit(1)
+//   })
