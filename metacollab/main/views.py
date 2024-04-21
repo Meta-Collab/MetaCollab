d8ec0e0b1960efcc5cb003615ddbb132fceb05bc
Nandika-A <nandikaagrawal610@gmail.com>
2024-04-21 11:40:45 +0530

    pushing late because of internet

--- metacollab/main/views.py
+++ metacollab/main/views.py
@@ -24,39 +24,48 @@ def get_room(request, roomuuid):
     context={
         "room" : roominfo,
         "members" : RoomToUser.objects.filter(room=roominfo),
-        "roomuuid":roomuuid
+        "roomuuid":roomuuid,
+
     }
     if request.method=='POST' :
         #federated_learning(request._request)
         print(request.POST.get('action'))
         if request.POST.get('action') == 'train':
             try:
+                # print("done3")
+                # received_url = 'https://gateway.pinata.cloud/ipfs/QmUTsRiseFSv76FSrVVuFEhZbpeQZ9wizB696vpYxRHxma' #data['url']
+                # roominfo.url=received_url
+                # roominfo.save()
+                print("done")
                 url="http://localhost:3000/upload_file"
                 response = requests.post(url, json={'file_path':'/home/nandika/MetaCollab/metacollab/fl/saved_models/mobilenetv2/fingerprint.pb', 'room_id':roomuuid})
                 response.raise_for_status()
                 print("see")
                 data = response.json()
-                received_url = data['url']
+                # url = 'https://gateway.pinata.cloud/ipfs/QmUTsRiseFSv76FSrVVuFEhZbpeQZ9wizB696vpYxRHxma' #data['url']
+                url=data['url']
                 cid = data['cid']
                 roominfo.url=url
                 roominfo.cid=cid
                 roominfo.save()
-                print(received_url,cid)
+                print(url,cid)
+                
+
             except:
                 print("Error in sending data to Node.js server")
         if request.POST.get('action') == 'submit':
             try:
                 updated_string=request.GET.get('string')
                 # Write the updated string to the file
-                #put string in gfg.txt path
-                # with open('gfg.txt', 'w') as file:
-                #     file.write(updated_string)
-                #from fl.client import main
-                #main(1)
+                # put string in gfg.txt path
+                with open('gfg.txt', 'w') as file:
+                    file.write(updated_string)
+                from fl.client import main
+                main(1)
                 # #get string from gfg.txt
-                #with open('/home/nandika/MetaCollab/metacollab/fl/gfg.txt', 'r') as file:
-                # # Read the entire contents of the file into a string
-                  #  complete_string = file.read()
+                with open('/home/nandika/MetaCollab/metacollab/fl/gfg.txt', 'r') as file:
+                # Read the entire contents of the file into a string
+                   complete_string = file.read()
                 put_string(updated_string,roomuuid)
                 #return string and put string in blockchain
             except:
@@ -183,9 +192,9 @@ def update_model(request,roomuuid):
 
         updated_string=request.GET.get('string')
         # Write the updated string to the file
-        #put string in gfg.txt path
-        # with open('gfg.txt', 'w') as file:
-        #     file.write(updated_string)
+        # put string in gfg.txt path
+        with open('gfg.txt', 'w') as file:
+            file.write(updated_string)
         from fl.client import main
         main(1)
         # #get string from gfg.txt
@@ -201,13 +210,13 @@ def get_commits(request,roomuuid):
     commits={}
     
     url="http://localhost:3000/get_commits"
-    # response = requests.post(url, json={'room_id': roomuuid})
-    # response.raise_for_status()
-    # data = response.json()
-    # print(data)
-    commits='[ 7.81871229e-02 -4.58515435e-01 -7.39910543e-01 -1.21237881e-01 1.18119540e-02  2.05976233e-01 -7.21554339e-01 -6.48166612e-02 8.36113170e-02  5.00397974e-22 -6.65314180e-22 -8.04441097e-23 -2.41206422e-01  1.55485459e-02 -1.80058792e-01 -5.77486038e-01 2.82645345e-01 -2.47149408e-01 -1.29563004e-01 -5.72289377e-02 -1.34234670e-23  1.42827420e-03  6.42313480e-01 -8.67906958e-02 -6.00320927e-04  2.15270277e-03  4.66955379e-02  3.41252493e-22 3.20650823e-03 -7.62496144e-02 -3.43744314e-21  5.17112970e-21 ]'
+    response = requests.post(url, json={'room_id': roomuuid})
+    response.raise_for_status()
+    data = response.json()
+    print(data)
+    #commits='[ 7.81871229e-02 -4.58515435e-01 -7.39910543e-01 -1.21237881e-01 1.18119540e-02  2.05976233e-01 -7.21554339e-01 -6.48166612e-02 8.36113170e-02  5.00397974e-22 -6.65314180e-22 -8.04441097e-23 -2.41206422e-01  1.55485459e-02 -1.80058792e-01 -5.77486038e-01 2.82645345e-01 -2.47149408e-01 -1.29563004e-01 -5.72289377e-02 -1.34234670e-23  1.42827420e-03  6.42313480e-01 -8.67906958e-02 -6.00320927e-04  2.15270277e-03  4.66955379e-02  3.41252493e-22 3.20650823e-03 -7.62496144e-02 -3.43744314e-21  5.17112970e-21 ]'
     # Retrieve 'commits' from the response
-    #commits = data.get('commits', [])
+    commits = data.get('commits', [])
     #commits = data['commits']
     #int(commits)
 
